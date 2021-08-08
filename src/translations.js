4e54ac2e4090278acd8db783b533254c7be8f709
Marlon Passos <63638196+MarlonPassos-git@users.noreply.github.com>
2021-08-08 07:15:50 -0300

    Adding translation to portuguese
    Specifically for Brazilian Portuguese, however it can also be understood by Portuguese speakers in Europe. If by chance you find it necessary, you can put "pt-br" in the object name instead of just "pt"

--- src/translations.js
+++ src/translations.js
@@ -3,205 +3,268 @@
  *
  * @type {object}
  */
-const TRANSLATIONS = {
-  de: {
-    challenge: {
-      statement: 'Aussage:',
+ const TRANSLATIONS = {
+    de: {
+      challenge: {
+        statement: 'Aussage:',
+      },
+      closest_solution: {
+        title: 'Ähnlichste Lösung:',
+      },
+      corrected_answer: {
+        title: 'Korrigierte Antwort:',
+      },
+      modal: {
+        close: 'Schließen',
+        fit_to_content: 'An Inhalt anpassen',
+        maximize: 'Maximieren',
+        minimize: 'Minimieren',
+      },
+      pagination: {
+        go_to_first: 'Zur ersten Seite gehen',
+        go_to_last: 'Zur letzten Seite gehen',
+        go_to_page: 'Zu Seite {{page}} gehen',
+        go_to_next: 'Zur nächsten Seite gehen',
+        go_to_previous: 'Zur vorherigen Seite gehen',
+      },
+      solution_link: {
+        label: 'Lösungen ({{count}})',
+      },
+      solution_list: {
+        all: 'alle',
+        alphabetical_sort: 'Alphabetisch sortiert',
+        correct_solutions: 'Richtige Lösungen:',
+        filter: 'Filter:',
+        no_matching_solution: 'Es gibt keine passende Lösung.',
+        per_page: 'pro Seite:',
+        similarity_sort: 'Nach Ähnlichkeit sortiert',
+        sort_ascending: 'In aufsteigender Reihenfolge sortieren',
+        sort_alphabetically: 'Alphabetisch sortieren',
+        sort_by_similarity: 'Nach Ähnlichkeit sortieren',
+        sort_descending: 'In absteigender Reihenfolge sortieren',
+        view_list_with_word: 'Lösungen mit "{{word}}" ansehen',
+        view_list_without_word: 'Lösungen ohne "{{word}}" ansehen',
+      },
+      user_reference: {
+        cancel: 'Abbrechen',
+        none: 'Noch keine',
+        pin: 'Anpinnen',
+        unpin: 'Pin entfernen',
+        update: 'Update',
+        your_reference: 'Deine Referenz:',
+      },
+      word_filter: {
+        absent: 'Nicht enthalten',
+        add_filter: 'Filter hinzufügen',
+        anywhere_in_solution: 'Irgendwo in der Lösung',
+        anywhere_in_word: 'Irgendwo in einem Wort',
+        at_solution_end: 'Am Ende der Lösung',
+        at_solution_start: 'Am Beginn der Lösung',
+        at_word_end: 'Am Ende eines Wortes',
+        at_word_start: 'Am Beginn eines Wortes',
+        click_to_remove_filter: 'Klicken um den Filter zu entfernen',
+        exact_solution: 'Exakte Lösung',
+        exact_word: 'Exaktes Wort',
+        present: 'Enthalten',
+      },
     },
-    closest_solution: {
-      title: 'Ähnlichste Lösung:',
+    en: {
+      challenge: {
+        statement: 'Statement:',
+      },
+      closest_solution: {
+        title: 'Closest solution:',
+      },
+      corrected_answer: {
+        title: 'Corrected answer:',
+      },
+      modal: {
+        close: 'Close',
+        fit_to_content: 'Fit to content',
+        maximize: 'Maximize',
+        minimize: 'Minimize',
+      },
+      pagination: {
+        go_to_first: 'Go to first page',
+        go_to_last: 'Go to last page',
+        go_to_page: 'Go to page {{page}}',
+        go_to_next: 'Go to next page',
+        go_to_previous: 'Go to previous page',
+      },
+      solution_link: {
+        label: 'Solutions ({{count}})',
+      },
+      solution_list: {
+        all: 'all',
+        alphabetical_sort: 'Alphabetical sort',
+        correct_solutions: 'Correct solutions:',
+        filter: 'Filter:',
+        no_matching_solution: 'There is no matching solution.',
+        per_page: 'per page:',
+        similarity_sort: 'Similarity sort',
+        sort_ascending: 'Sort in ascending order',
+        sort_alphabetically: 'Sort alphabetically',
+        sort_by_similarity: 'Sort by similarity',
+        sort_descending: 'Sort in descending order',
+        view_list_with_word: 'View solutions with "{{word}}"',
+        view_list_without_word: 'View solutions without "{{word}}"',
+      },
+      user_reference: {
+        cancel: 'Cancel',
+        none: 'None yet',
+        pin: 'Pin',
+        unpin: 'Unpin',
+        update: 'Update',
+        your_reference: 'Your reference:',
+      },
+      word_filter: {
+        absent: 'Absent',
+        add_filter: 'Add a filter',
+        anywhere_in_solution: 'Anywhere in the solution',
+        anywhere_in_word: 'Anywhere in a word',
+        at_solution_end: 'At the end of the solution',
+        at_solution_start: 'At the start of the solution',
+        at_word_end: 'At the end of a word',
+        at_word_start: 'At the start of a word',
+        click_to_remove_filter: 'Click to remove filter',
+        exact_solution: 'Exact solution',
+        exact_word: 'Exact word',
+        present: 'Present',
+      },
+      pt: {
+        challenge: {
+          statement: 'Frase de referencia:',
+        },
+        closest_solution: {
+          title: 'Soluções parecidas:',
+        },
+        corrected_answer: {
+          title: 'Resposta correta:',
+        },
+        modal: {
+          close: 'Fechar',
+          fit_to_content: 'Ajustar a tela',
+          maximize: 'Maximizar',
+          minimize: 'Minimizar',
+        },
+        pagination: {
+          go_to_first: 'Vá para a primeira pagina',
+          go_to_last: 'Vá para a ultima pagina',
+          go_to_page: 'Vá para a página {{page}}',
+          go_to_next: 'Vá para a proxima pagina',
+          go_to_previous: 'Vá para a anterior pagina',
+        },
+        solution_link: {
+          label: 'Soluções ({{count}})',
+        },
+        solution_list: {
+          all: 'Todas',
+          alphabetical_sort: 'Ordem alfabética',
+          correct_solutions: 'Soluções corretas:',
+          filter: 'Filtrar:',
+          no_matching_solution: 'Não há solução compativel',
+          per_page: 'por página:',
+          similarity_sort: 'Ordem semelhante',
+          sort_ascending: 'Classifique por ordem crescente',
+          sort_alphabetically: 'Classifique por ordem alfabética',
+          sort_by_similarity: 'Classifique por similaridade',
+          sort_descending: 'Classifique por ordem decrescente',
+          view_list_with_word: 'Veja Soluções com "{{word}}"',
+          view_list_without_word: 'Veja soluções sem "{{word}}"',
+        },
+        user_reference: {
+          cancel: 'Cancelar',
+          none: 'Nenhum ainda',
+          pin: 'Fixar',
+          unpin: 'Desfixar',
+          update: 'Atualizar',
+          your_reference: 'Sua referencia:',
+        },
+        word_filter: {
+          absent: 'Nao Tem/Contem',
+          add_filter: 'Adicionar um filtro',
+          anywhere_in_solution: 'Em qualquer lugar da solução',
+          anywhere_in_word: 'em qualquer lugar em uma palavra',
+          at_solution_end: 'No final da solução',
+          at_solution_start: 'No início da solução',
+          at_word_end: 'A frase termina com a palavra',
+          at_word_start: 'A frase inicia com a palavra',
+          click_to_remove_filter: 'Clique para remover o filtro',
+          exact_solution: 'Solução certa',
+          exact_word: 'Palavra certa',
+          present: 'Tem/Contem',
+        },
     },
-    corrected_answer: {
-      title: 'Korrigierte Antwort:',
+    fr: {
+      challenge: {
+        statement: 'Énoncé :',
+      },
+      closest_solution: {
+        title: 'Solution la plus proche :',
+      },
+      corrected_answer: {
+        title: 'Réponse corrigée :',
+      },
+      modal: {
+        close: 'Fermer',
+        fit_to_content: 'Adapter au contenu',
+        maximize: 'Maximiser',
+        minimize: 'Minimiser',
+      },
+      pagination: {
+        go_to_first: 'Aller à la première page',
+        go_to_last: 'Aller à la dernière page',
+        go_to_page: 'Aller à la page {{page}}',
+        go_to_next: 'Aller à la page suivante',
+        go_to_previous: 'Aller à la page précédente',
+      },
+      solution_link: {
+        label: 'Solutions ({{count}})',
+      },
+      solution_list: {
+        all: 'tout',
+        alphabetical_sort: 'Tri alphabétique',
+        correct_solutions: 'Solutions correctes :',
+        filter: 'Filtrer:',
+        no_matching_solution: 'Il n\'y a aucune solution correspondante.',
+        per_page: 'par page :',
+        similarity_sort: 'Tri par similarité',
+        sort_ascending: 'Trier par ordre croissant',
+        sort_alphabetically: 'Trier alphabétiquement',
+        sort_by_similarity: 'Trier par similarité',
+        sort_descending: 'Trier par ordre décroissant',
+        view_list_with_word: 'Voir les solutions avec "{{word}}"',
+        view_list_without_word: 'Voir les solutions sans "{{word}}"',
+      },
+      user_reference: {
+        cancel: 'Annuler',
+        none: 'Aucune pour l\'instant',
+        pin: 'Épingler',
+        unpin: 'Désépingler',
+        update: 'Modifier',
+        your_reference: 'Ta référence :',
+      },
+      word_filter: {
+        absent: 'Absent',
+        add_filter: 'Ajouter un filtre',
+        anywhere_in_solution: 'N\'importe où dans la solution',
+        anywhere_in_word: 'N\'importe où dans un mot',
+        at_solution_end: 'À la fin de la solution',
+        at_solution_start: 'Au début de la solution',
+        at_word_end: 'À la fin d\'un mot',
+        at_word_start: 'Au début d\'un mot',
+        click_to_remove_filter: 'Cliquer pour enlever le filtre',
+        exact_solution: 'Solution exacte',
+        exact_word: 'Mot exact',
+        present: 'Présent',
+      },
     },
-    modal: {
-      close: 'Schließen',
-      fit_to_content: 'An Inhalt anpassen',
-      maximize: 'Maximieren',
-      minimize: 'Minimieren',
-    },
-    pagination: {
-      go_to_first: 'Zur ersten Seite gehen',
-      go_to_last: 'Zur letzten Seite gehen',
-      go_to_page: 'Zu Seite {{page}} gehen',
-      go_to_next: 'Zur nächsten Seite gehen',
-      go_to_previous: 'Zur vorherigen Seite gehen',
-    },
-    solution_link: {
-      label: 'Lösungen ({{count}})',
-    },
-    solution_list: {
-      all: 'alle',
-      alphabetical_sort: 'Alphabetisch sortiert',
-      correct_solutions: 'Richtige Lösungen:',
-      filter: 'Filter:',
-      no_matching_solution: 'Es gibt keine passende Lösung.',
-      per_page: 'pro Seite:',
-      similarity_sort: 'Nach Ähnlichkeit sortiert',
-      sort_ascending: 'In aufsteigender Reihenfolge sortieren',
-      sort_alphabetically: 'Alphabetisch sortieren',
-      sort_by_similarity: 'Nach Ähnlichkeit sortieren',
-      sort_descending: 'In absteigender Reihenfolge sortieren',
-      view_list_with_word: 'Lösungen mit "{{word}}" ansehen',
-      view_list_without_word: 'Lösungen ohne "{{word}}" ansehen',
-    },
-    user_reference: {
-      cancel: 'Abbrechen',
-      none: 'Noch keine',
-      pin: 'Anpinnen',
-      unpin: 'Pin entfernen',
-      update: 'Update',
-      your_reference: 'Deine Referenz:',
-    },
-    word_filter: {
-      absent: 'Nicht enthalten',
-      add_filter: 'Filter hinzufügen',
-      anywhere_in_solution: 'Irgendwo in der Lösung',
-      anywhere_in_word: 'Irgendwo in einem Wort',
-      at_solution_end: 'Am Ende der Lösung',
-      at_solution_start: 'Am Beginn der Lösung',
-      at_word_end: 'Am Ende eines Wortes',
-      at_word_start: 'Am Beginn eines Wortes',
-      click_to_remove_filter: 'Klicken um den Filter zu entfernen',
-      exact_solution: 'Exakte Lösung',
-      exact_word: 'Exaktes Wort',
-      present: 'Enthalten',
-    },
-  },
-  en: {
-    challenge: {
-      statement: 'Statement:',
-    },
-    closest_solution: {
-      title: 'Closest solution:',
-    },
-    corrected_answer: {
-      title: 'Corrected answer:',
-    },
-    modal: {
-      close: 'Close',
-      fit_to_content: 'Fit to content',
-      maximize: 'Maximize',
-      minimize: 'Minimize',
-    },
-    pagination: {
-      go_to_first: 'Go to first page',
-      go_to_last: 'Go to last page',
-      go_to_page: 'Go to page {{page}}',
-      go_to_next: 'Go to next page',
-      go_to_previous: 'Go to previous page',
-    },
-    solution_link: {
-      label: 'Solutions ({{count}})',
-    },
-    solution_list: {
-      all: 'all',
-      alphabetical_sort: 'Alphabetical sort',
-      correct_solutions: 'Correct solutions:',
-      filter: 'Filter:',
-      no_matching_solution: 'There is no matching solution.',
-      per_page: 'per page:',
-      similarity_sort: 'Similarity sort',
-      sort_ascending: 'Sort in ascending order',
-      sort_alphabetically: 'Sort alphabetically',
-      sort_by_similarity: 'Sort by similarity',
-      sort_descending: 'Sort in descending order',
-      view_list_with_word: 'View solutions with "{{word}}"',
-      view_list_without_word: 'View solutions without "{{word}}"',
-    },
-    user_reference: {
-      cancel: 'Cancel',
-      none: 'None yet',
-      pin: 'Pin',
-      unpin: 'Unpin',
-      update: 'Update',
-      your_reference: 'Your reference:',
-    },
-    word_filter: {
-      absent: 'Absent',
-      add_filter: 'Add a filter',
-      anywhere_in_solution: 'Anywhere in the solution',
-      anywhere_in_word: 'Anywhere in a word',
-      at_solution_end: 'At the end of the solution',
-      at_solution_start: 'At the start of the solution',
-      at_word_end: 'At the end of a word',
-      at_word_start: 'At the start of a word',
-      click_to_remove_filter: 'Click to remove filter',
-      exact_solution: 'Exact solution',
-      exact_word: 'Exact word',
-      present: 'Present',
-    },
-  },
-  fr: {
-    challenge: {
-      statement: 'Énoncé :',
-    },
-    closest_solution: {
-      title: 'Solution la plus proche :',
-    },
-    corrected_answer: {
-      title: 'Réponse corrigée :',
-    },
-    modal: {
-      close: 'Fermer',
-      fit_to_content: 'Adapter au contenu',
-      maximize: 'Maximiser',
-      minimize: 'Minimiser',
-    },
-    pagination: {
-      go_to_first: 'Aller à la première page',
-      go_to_last: 'Aller à la dernière page',
-      go_to_page: 'Aller à la page {{page}}',
-      go_to_next: 'Aller à la page suivante',
-      go_to_previous: 'Aller à la page précédente',
-    },
-    solution_link: {
-      label: 'Solutions ({{count}})',
-    },
-    solution_list: {
-      all: 'tout',
-      alphabetical_sort: 'Tri alphabétique',
-      correct_solutions: 'Solutions correctes :',
-      filter: 'Filtrer:',
-      no_matching_solution: 'Il n\'y a aucune solution correspondante.',
-      per_page: 'par page :',
-      similarity_sort: 'Tri par similarité',
-      sort_ascending: 'Trier par ordre croissant',
-      sort_alphabetically: 'Trier alphabétiquement',
-      sort_by_similarity: 'Trier par similarité',
-      sort_descending: 'Trier par ordre décroissant',
-      view_list_with_word: 'Voir les solutions avec "{{word}}"',
-      view_list_without_word: 'Voir les solutions sans "{{word}}"',
-    },
-    user_reference: {
-      cancel: 'Annuler',
-      none: 'Aucune pour l\'instant',
-      pin: 'Épingler',
-      unpin: 'Désépingler',
-      update: 'Modifier',
-      your_reference: 'Ta référence :',
-    },
-    word_filter: {
-      absent: 'Absent',
-      add_filter: 'Ajouter un filtre',
-      anywhere_in_solution: 'N\'importe où dans la solution',
-      anywhere_in_word: 'N\'importe où dans un mot',
-      at_solution_end: 'À la fin de la solution',
-      at_solution_start: 'Au début de la solution',
-      at_word_end: 'À la fin d\'un mot',
-      at_word_start: 'Au début d\'un mot',
-      click_to_remove_filter: 'Cliquer pour enlever le filtre',
-      exact_solution: 'Solution exacte',
-      exact_word: 'Mot exact',
-      present: 'Présent',
-    },
-  },
-};
-
-/**
- * @param {string} languageTag A language tag.
- * @returns {object} The available translations for the given language tag.
- */
-export const getTranslations = languageTag => (
-  TRANSLATIONS[languageTag] || TRANSLATIONS[languageTag.substring(0, 2)] || {}
-);
+  };
+  
+  /**
+   * @param {string} languageTag A language tag.
+   * @returns {object} The available translations for the given language tag.
+   */
+  export const getTranslations = languageTag => (
+    TRANSLATIONS[languageTag] || TRANSLATIONS[languageTag.substring(0, 2)] || {}
+  );
